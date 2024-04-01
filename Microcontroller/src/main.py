095e6409edcc53eb8c959b518b830929f8fe05f4
JayTheAxolotl <jacobmine2010@gmail.com>
2024-04-01 18:46:04 -0500

    Unintentionally changed the hierarchy

---
+++ Microcontroller/src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       johnt                                                        #
+# 	Created:      4/1/2024, 6:23:50 PM                                         #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        
